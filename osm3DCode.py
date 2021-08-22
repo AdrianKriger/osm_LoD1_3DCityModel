0b44bd20955beb6ed638c87274ab9b2b28747b57
Adrian Kriger <59996720+AdrianKriger@users.noreply.github.com>
2021-08-22 23:15:20 +0200

    word

--- osm3DCode.py
+++ osm3DCode.py
@@ -7,7 +7,7 @@ elevation via a raster DEM.
 @author: arkriger - July 2021
 @github: https://github.com/AdrianKriger/osm_LoD1_3DCityModel
 
-some script credit:
+script credit:
     - building height from osm building:level: https://github.com/ualsg/hdb3d-code/blob/master/hdb2d.py - Filip Biljecki <filip@nus.edu.sg>
     - polygon to lines without duplicate edges: https://gis.stackexchange.com/questions/236903/converting-polygon-to-lines-without-duplicate-edges
     - geopandas snap routine: https://gis.stackexchange.com/questions/290092/how-to-do-snapping-in-geopandas
