414d164b42657a457d28477f5ba191ef112f75a2
Adrian Kriger <59996720+AdrianKriger@users.noreply.github.com>
2023-08-05 18:21:43 +0200

    sajg-Aug2023

--- village_campus/osm3DMain.py
+++ village_campus/osm3DMain.py
@@ -2,12 +2,13 @@
 # env/osm3D_vc-env
 ######################
 # main() for osm3DCode
-# author: arkriger - July 2022
+# author: arkriger - July 2023
 # github: https://github.com/AdrianKriger/osm_LoD1_3DCityModel
 #####################
 
 import os
 import sys
+import keyboard
 import json
 
 from osgeo import gdal
@@ -15,24 +16,25 @@ from osgeo import gdal
 import time
 from datetime import timedelta
 
-from osm3DCode import (requestOsmBld, requestOsmAoi, getosmArea, 
-                        prepareDEM, createXYZ, 
-                        requestOsmRoads, prepareRoads, 
-                        assignZ, writegjson, prep_Skygjson, prep_roof, getosmBld,
-                        getXYZ, 
-                        getBldVertices, createSgmts, appendCoords, 
-                        getRdVertices, requestOsmParking, 
-                        getAOIVertices, 
-                        executeDelaunay, pvPlot, 
-                        output_cityjsonR, 
-                        output_cityjson, 
-                        writeObj, write275obj)
+from osm3DCode_sagc_v01a import (requestOsmBld, requestOsmAoi, getOsmArea, 
+                                 prepareDEM, createXYZ, 
+                                 assignZ, writegjson, 
+                                 getXYZ, 
+                                 mtPlot02, getOsmBld,
+                                 getBldVertices, createSgmts, concatCoords, getAOIVertices, 
+                                 executeDelaunay, 
+                                 outputCityjsonB)
     
 def main():
     start = time.time()
     
+    
     try:
-        jparams = json.load(open('osm3Dcput_param.json'))
+
+        #jparams = json.load(open('osm3DuEstate_param.json'))
+        #jparams = json.load(open('osm3Dcput_param.json'))
+        jparams = json.load(open('osm3Dmamre_param.json'))
+
     except:
         print("ERROR: something is wrong with the param.json file.")
         sys.exit()
@@ -44,7 +46,7 @@ def main():
     
     ts = requestOsmBld(jparams)
     aoi = requestOsmAoi(jparams)
-    aoi, aoibuffer, extent = getosmArea(aoi, jparams['aoi'], jparams['osm_type'], jparams['crs'])
+    aoi, aoibuffer, extent = getOsmArea(aoi, jparams['aoi'], jparams['osm_type'], jparams['crs'])
        
     path = os.getcwd()
     r_name = 'result'
@@ -61,75 +63,68 @@ def main():
     #gt_reverse=gdal.InvGeoTransform(gt_forward)
     rb = src_ds.GetRasterBand(1)
     
-    if jparams['roads'] == "Yes":
-        rd = requestOsmRoads(jparams)
-        pk = requestOsmParking(jparams)
-        one, hsr = prepareRoads(jparams, rd, pk, aoi, aoibuffer, gt_forward, rb)
-    
-    ts, skywalk, roof = assignZ(ts, gt_forward, rb) 
-    writegjson(ts, jparams)
-    if len(skywalk) > 0:
-        skywalk = prep_Skygjson(skywalk, jparams)
-    if len(roof) > 0:
-        roof = prep_roof(roof, jparams)
-    
-    dis, hs = getosmBld(jparams)
-    
-    if jparams['roads'] == "Yes":
-        hs2 = hs.copy()
-        hs = hs.append(hsr)
-        dis_c = dis.copy()
-        dis_c = dis_c.append(one)
-        gdf = getXYZ(dis_c, aoibuffer, jparams)
-    else:
-        gdf = getXYZ(dis, aoibuffer, jparams)
-    
-    ac, c, min_zbld = getBldVertices(dis, gt_forward, rb)
-    idx = []
-    idx, idx01 = createSgmts(ac, c, gdf, idx)
-    df2 = appendCoords(gdf, ac)
-    
-    if jparams['roads'] == "Yes":
-         acoi, ca = getAOIVertices(aoi, gt_forward, rb) 
-         idx, idx01 = createSgmts(acoi, ca, df2, idx)
-         df3 = appendCoords(df2, acoi)
-         
-         t_list, rd_pts, acr, cr = getRdVertices(one, idx01, acoi, hs2, gt_forward, rb)
-         idx, idx01 = createSgmts(acr, cr, df3, idx)
-         df4 = appendCoords(df3, acr)
-    else:
-        acoi, ca = getAOIVertices(aoi, gt_forward, rb)
-        idx, idx01 = createSgmts(acoi, ca, df2, idx)
-        df4 = appendCoords(df2, acoi)
-        
-    src_ds = None
-        
-    pts = df4[['x', 'y', 'z']].values
-    ##-- change the dtype to 'float64'
-    pts = pts.astype('float64')
+    mtPlot02(ts, jparams)
+    print('')
+    print("Have a look at the matplotlib render that might highlight errors (red) in the 2D vectors.\n\nIf there are no challenges press 'Enter' to continue; else press 'Esc' to exit: ")
+    while True: 
+        try:
+            if keyboard.is_pressed('ENTER'):
+                print('')
+                print("\nYou pressed Enter... we continue with osm_LoD1_3DCityModel.")
+ 
+                ts = assignZ(ts, gt_forward, rb)
+                writegjson(ts, jparams)
+                
+                dis, hs, result = getOsmBld(jparams)
 
-    t = executeDelaunay(hs, df4, idx)
-    
-    ##-- check terrain with a plot
-    pvPlot(t, pts, idx, hs)
+                gdf = getXYZ(dis, aoibuffer, jparams)
+                
+                ac, c, min_zbld = getBldVertices(dis, gt_forward, rb)
+                idx = []
+                idx, idx01 = createSgmts(ac, c, gdf, idx)
+                df2 = concatCoords(gdf, ac)
 
-    minz = df4['z'].min()
-    maxz = df4['z'].max()
-    
-    #writeObj(pts, t, 'wvft_cput3d.obj') ~ this will write the terrain surface only
-    if jparams['roads'] == "Yes":
-        output_cityjsonR(extent, minz, maxz, t, pts, t_list, rd_pts, jparams, min_zbld, skywalk, roof, acoi)
-    else: 
-        output_cityjson(extent, minz, maxz, t, pts, jparams, min_zbld, skywalk, roof)
-    
-    write275obj(jparams)
-        
+                acoi, ca = getAOIVertices(aoi, gt_forward, rb)
+                idx, idx01 = createSgmts(acoi, ca, df2, idx)
+                df4 = concatCoords(df2, acoi)
+                            
+                pts = df4[['x', 'y', 'z']].values
+                pts = pts.astype('float64')
+            
+                t = executeDelaunay(hs, df4, idx)
+                           
+                minz = df4['z'].min()
+                maxz = df4['z'].max()
+                
+                outputCityjsonB(extent, minz, maxz, t, pts, jparams, min_zbld, result)
+                break
+                
+            if keyboard.is_pressed('Esc'):
+            #else:
+                print("\nyou pressed Esc, we exit.\n\nA .png, highlighing errors, has been saved to the 'data' folder.\n\nPlease fix these challenges with your favorite OpenStreetMap editor.")
+                sys.exit(0)
+        except:
+            break
+     
+    src_ds = None      
     end = time.time()
-    print('runtime:', str(timedelta(seconds=(end - start))))
-    
-     #-- cput runtime: 0:00:38.592168 ~ university campus: 50 buildings / with 57 roads: 0:04:16.716635
-     #-- rural runtime: 0:12:51.139945 ~ rural village: population 9 000 / with 160 roads: 0:31:50.480651
-     #-- neighbourhood runtime: 0:00:41.587389 ~ urban neighbourhood: population ~ 1 000, 305 buildings / with 57 roads: 0:04:34.000448
+    print('runtime:', str(timedelta(seconds=(end - start))))  
+   
+   #--25-meter DEM
+   #-- cput runtime: 0:00:21.795588 ~ university campus: 50 buildings
+   #-- rural runtime: 0:16:00.278584 ~ rural village: population 9 000, 2159 buildings
+   #-- neighbourhood runtime: 0:01:12.826543 ~ urban neighbourhood: population ~ 1 000, 305 buildings 
+
+   #--10-metre DEM
+   #-- cput runtime: 0:01:29.233388 ~ university campus: 50 buildings / 
+   #-- rural runtime: 1:09:24.040366 ~ rural village: population 9 000, 2159 buildings /
+   #-- neighbourhood runtime: 0:01:43.012501 ~ urban neighbourhood: population ~ 1 000, 305 buildings /
+   
+   #--5-metre DEM
+   #-- cput runtime: 0:05:12.451642 ~ university campus: 50 buildings / 
+   #-- rural runtime:               ~ rural village: population 9 000 /
+   #-- neighbourhood runtime: 0:07:29.521754 ~ urban neighbourhood: population ~ 1 000, 305 buildings /
+   
 
 if __name__ == "__main__":
     main()
